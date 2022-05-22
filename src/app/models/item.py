867cc8ec9f3fa7346bfc224cd5c8d67dcdb22fc2
Peter Schutt <peter@topsport.com.au>
2022-05-22 13:35:00 +1000

    Remove relationships.
    For #11.
    
    Removing these for now. I need to spend some time working on what
    the best pattern is for defining eager loading on relationships.
    Until them, I'm just removing so I can get the app running.

--- src/app/models/item.py
+++ src/app/models/item.py
@@ -11,5 +11,3 @@ from .mixins import DateFieldsMixins
 class Item(DateFieldsMixins, Base):
     name: str = Column(String(64), nullable=False)
     owner_id: uuid.UUID = Column(UUID(as_uuid=True), ForeignKey("user.id"))
-
-    owner = relationship("User", back_populates="items")
