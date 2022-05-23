909de04ec735459c2a955810ee4d3e864b1ca5e6
Peter Schutt <peter@topsport.com.au>
2022-05-23 14:46:21 +1000

    Move decorated SQLAlchemy UUID type into separate module.

--- src/app/models/base.py
+++ src/app/models/base.py
@@ -1,25 +1,10 @@
 import uuid
-from typing import Any
 
 from pydantic import BaseModel as _BaseModel
 from sqlalchemy import Column
-from sqlalchemy.dialects import postgresql as pg
 from sqlalchemy.orm.decl_api import as_declarative, declared_attr
-from sqlalchemy.types import TypeDecorator
 
-
-class UUID(TypeDecorator):
-
-    cache_ok = True
-    impl = pg.UUID
-
-    def process_bind_param(self, value: "UUID | None", dialect: Any) -> Any:
-        return str(value)
-
-    def process_result_value(self, value: Any, dialect: Any) -> uuid.UUID | None:
-        if value is None:
-            return None
-        return uuid.UUID(value)
+from .sa_uuid import UUID
 
 
 @as_declarative()
---
+++ src/app/models/sa_uuid.py
@@ -0,0 +1,25 @@
+import uuid
+from typing import Any
+
+from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.types import TypeDecorator
+
+
+class UUID(TypeDecorator):
+    """
+    Decorated `sqlalchemy.dialects.postgresql.UUID` type that ensures we are only
+    dealing with stdlib's `uuid.UUID` outsdide of SQLAlchemy.
+
+    This allows the type to play nicely with orjson.
+    """
+
+    cache_ok = True
+    impl = pg.UUID
+
+    def process_bind_param(self, value: "UUID | None", dialect: Any) -> Any:
+        return str(value)
+
+    def process_result_value(self, value: Any, dialect: Any) -> uuid.UUID | None:
+        if value is None:
+            return None
+        return uuid.UUID(value)
