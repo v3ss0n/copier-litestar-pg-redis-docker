33f0cb359299e782c3190587953b133a9cf794bf
Peter Schutt <peter@topsport.com.au>
2022-05-23 15:54:38 +1000

    Remove unused ItemRepository.

--- src/app/repositories/__init__.py
+++ src/app/repositories/__init__.py
@@ -1,3 +1,2 @@
 # flake8: noqa
-from .item import ItemRepository
 from .user import UserRepository
--- src/app/repositories/item.py
+++
@@ -1,7 +0,0 @@
-from app.models import Item
-
-from .base import AbstractBaseRepository
-
-
-class ItemRepository(AbstractBaseRepository[Item]):
-    model = Item
