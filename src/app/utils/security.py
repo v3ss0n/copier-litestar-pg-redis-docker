200f60e6d4389f28cfe607b8c0592cffbae583ed
Peter Schutt <peter@topsport.com.au>
2022-05-23 14:51:39 +1000

    Remove unused api.

--- src/app/utils/security.py
+++ src/app/utils/security.py
@@ -1,14 +1,6 @@
-from typing import cast
-
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-ALGORITHM = "HS256"
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return cast(bool, pwd_context.verify(plain_password, hashed_password))
 
 
 def get_password_hash(password: str) -> str:
