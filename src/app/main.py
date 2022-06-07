477069303a84c19a8bc10b1caeeb123de16b94c9
Peter Schutt <peter@topsport.com.au>
2022-06-08 08:28:11 +1000

    Custom exception handler for 500s that logs.
    This gives crucial debugging info at runtime, and also stderr is shown by pytest in event of failing test.

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,17 @@
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 from starlite import Starlite
 from starlite.plugins.sql_alchemy import SQLAlchemyPlugin
 
-from app import api, cache, db, health, openapi
+from app import api, cache, db, exceptions, health, openapi
 from app.config import app_settings, log_config
 
 app = Starlite(
     after_request=db.session_after_request,
     cache_config=cache.config,
     debug=app_settings.DEBUG,
+    exception_handlers={
+        HTTP_500_INTERNAL_SERVER_ERROR: exceptions.logging_exception_handler
+    },
     on_shutdown=[db.on_shutdown, cache.on_shutdown],
     on_startup=[log_config.configure],
     openapi_config=openapi.config,
